72667a26dcd01988de39b423fb43e949975a07e5
CaioMariano <caio.araujo.mariano@hotmail.com>
2018-11-22 20:22:38 -0200

    Initial Commit

---
+++ IdDog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IdDog
+//
+//  Created by Caio Araujo Mariano on 22/11/2018.
+//  Copyright © 2018 Caio Araujo Mariano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
